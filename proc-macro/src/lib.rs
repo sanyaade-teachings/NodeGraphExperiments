59c20dfe1c5652db65b47be5a82c3564a27f1fab
Dennis Kobert <dennis@kobert.dev>
2021-07-08 17:25:02 +0200

    Create composition demo with type checking

--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -1,30 +1,73 @@
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
-use syn::ItemFn;
+use quote::{quote, ToTokens};
+use syn::punctuated::Punctuated;
+use syn::{parse_macro_input,  FnArg, ItemFn, Pat,  Type};
+
+fn extract_type(a: FnArg) -> Box<Type> {
+    match a {
+        FnArg::Typed(p) => p.ty, // notice `ty` instead of `pat`
+        _ => panic!("Not supported on types with `self`!"),
+    }
+}
+
+fn extract_arg_types(fn_args: Punctuated<FnArg, syn::token::Comma>) -> Vec<Box<Type>> {
+    return fn_args.into_iter().map(extract_type).collect::<Vec<_>>();
+}
+
+fn extract_arg_idents(fn_args: Punctuated<FnArg, syn::token::Comma>) -> Vec<Box<Pat>> {
+    return fn_args.into_iter().map(extract_arg_pat).collect::<Vec<_>>();
+}
+
+fn extract_arg_pat(a: FnArg) -> Box<Pat> {
+    match a {
+        FnArg::Typed(p) => p.pat,
+        _ => panic!("Not supported on types with `self`!"),
+    }
+}
 
 #[proc_macro_attribute] // 2
-pub fn to_string(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
+pub fn to_node(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let string = item.to_string();
     let item2 = item.clone();
     let parsed = parse_macro_input!(item2 as ItemFn); // 3
-    item.extend(generate_to_string(parsed, string)); // 4
-    item
+                                                      //item.extend(generate_to_string(parsed, string)); // 4
+                                                      //item
+    generate_to_string(parsed, string)
 }
 
 fn generate_to_string(parsed: ItemFn, string: String) -> TokenStream {
-    /*let fn_body = parsed.block; // function body
+    let whole_function = parsed.clone();
+    //let fn_body = parsed.block; // function body
     let sig = parsed.sig; // function signature
-    let vis = parsed.vis; // visibility, pub or not
+    //let vis = parsed.vis; // visibility, pub or not
     let fn_args = sig.inputs; // comma separated args
     let fn_return_type = sig.output; // return type
-    */
-    let fn_name = parsed.sig.ident; // function name/identifier
-    let new_name = fn_name.to_string() + "_to_string";
-    let new_name = syn::Ident::new(&new_name, proc_macro2::Span::call_site());
-    let x = quote::quote! {
-        fn #new_name() -> &'static str {
-          #string
+    let fn_name = sig.ident; // function name/identifier
+    let idents = extract_arg_idents(fn_args.clone());
+    let types = extract_arg_types(fn_args);
+    let types = types.iter().map(|t| t.to_token_stream()).collect::<Vec<_>>();
+    let idents = idents.iter().map(|t| t.to_token_stream()).collect::<Vec<_>>();
+
+    let return_type_string = fn_return_type.to_token_stream().to_string().replace("->","");
+    let arg_type_string = types.iter().map(|t|t.to_string()).collect::<Vec<_>>().join(", ");
+    let error = format!("called {} with the wrong type", fn_name.to_string());
+
+    let x = quote! {
+        fn #fn_name() -> Node {
+            Node { func: Box::new(move |x| {
+               let  args = x.downcast_ref::<(#(#types,)*)>().expect(#error);
+               let  (#(#idents,)*) = args;
+               #whole_function
+
+               Box::new(#fn_name(#(*#idents,)*))
+            }),
+            code:  #string.to_string(),
+             return_type: #return_type_string.trim().to_string(),
+             args: format!("({})",#arg_type_string.trim()),
+            }
         }
+
     };
+    //panic!("{}\n{:?}", x.to_string(), x);
     x.into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,61 @@
 use ide::{Analysis, DiagnosticsConfig};
+use std::any::Any;
 
-
-unsafe fn unsafe_compose<F: 'static, G: 'static, Fv, Fo, Gv, V>(g: G, f: F, ) -> Box<dyn Fn(Fv) -> V>
-where
-    F: Fn(Fv) -> Fo,
-    G: Fn(Gv) -> V,
-{
-    Box::new(move |x| g(std::mem::transmute_copy::<Fo, Gv>(&f(x))))
+struct Node {
+    func: Box<dyn Fn(Box<dyn Any>) -> Box<dyn Any>>,
+    code: String,
+    return_type: String,
+    args: String,
 }
 
 
-#[graph_proc_macros::to_string]
-fn gen_int(_: ()) -> u32 {
-    42
+fn compose(g: Node, f: Node) -> Node {
+    let Node{func, code,  args, return_type} = f;
+    let ffunc = func;
+    let fcode = code;
+    let freturn = return_type;
+    let fargs = args;
+    let Node{func, code, args, return_type, ..} = g;
+    assert_eq!(args, freturn);
+    Node{
+        func: Box::new(move |x| func(ffunc(x))),
+        code: fcode + code.as_str(), // temporary TODO: replace
+        return_type,
+        args: fargs,
+    }
 }
 
-#[graph_proc_macros::to_string]
-fn format_int(x: u32) -> String {
-    x.to_string()
+#[graph_proc_macros::to_node]
+fn gen_int() -> (u32, u32) {
+    (42,43)
+}
+#[graph_proc_macros::to_node]
+fn format_int(x: u32, y: u32) -> String {
+    x.to_string() + y.to_string().as_str()
 }
 
-pub fn check_code(code: String)  {
+
+pub fn check_code(code: String) {
     let (analysis, file_id) = Analysis::from_single_file(code);
     let config = DiagnosticsConfig::default();
 
-    let diagnostics: Vec<_> =analysis
+    let diagnostics: Vec<_> = analysis
         .diagnostics(&config, ide::AssistResolveStrategy::All, file_id)
         .unwrap()
         .into_iter()
         .map(|d| d.message)
         .collect();
 
-        println!("dig: {:#?}", diagnostics);
+    println!("dig: {:#?}", diagnostics);
 }
 
 fn main() {
-    let comp = unsafe { unsafe_compose(format_int, gen_int) };
-    println!("{}", comp(()));
-    check_code(format_int_to_string().to_string());
+    let gen_int = gen_int();
+    let format_int = format_int();
+    let comp = compose(format_int, gen_int);
+
+    println!("{:?}", (comp.func)(Box::new(())).downcast_ref::<String>());
+    /*let function = TEST_FN;
+    println!("{}", function);
+    check_code(function.to_string());*/
 }
